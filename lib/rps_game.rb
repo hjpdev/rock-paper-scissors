358399c051786f7a651a0c8036c9eb2358179b82
Harry <harryjp1995@gmail.com>
2018-08-09 13:24:52 +0100

    Refactored code & improved tests.

--- lib/rps_game.rb
+++ lib/rps_game.rb
@@ -2,18 +2,48 @@ class RockPaperScissors
   attr_reader :player_name
 
   def initialize
-    @player_name = ""
+    @player_name = ''
   end
 
   def get_name
-    puts "Enter name: "
+    puts 'Enter name: '
     @player_name = gets.chomp
+    puts "Hello #{@player_name},"
   end
 
   def user_input
-    puts "Choose \'rock\', \'paper\' or \'scissors\':"
-    user_input = gets.chomp
-    user_input
+    puts "choose \'rock\', \'paper\' or \'scissors\':"
+    input = gets.chomp
+    input =~ /[rock|paper|scissors]/ ? input : user_input
+  end
+
+  def computer_choice
+    c_choice = %w[rock paper scissors].sample
+    p "Computer choice: #{c_choice}."
+    c_choice
+  end
+
+  def result(user_input, c_choice)
+    if user_input == 'rock' && c_choice == 'scissors'
+      'You win.'
+    elsif user_input == 'scissors' && c_choice == 'paper'
+      'You win.'
+    elsif user_input == 'paper' && c_choice == 'rock'
+      'You win.'
+    elsif user_input == c_choice
+      'Tie.'
+    else
+      'You lose.'
+    end 
+  end
+
+  def game
+    get_name
+    u = user_input
+    c = computer_choice
+    p result(u, c)
   end
 end
 
+rps = RockPaperScissors.new
+rps.game
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -1,15 +1,55 @@
-require 'rps_game'
+require 'rps_game.rb'
 
 describe RockPaperScissors do
-  it "Should take the user's name when #get_name is called." do
+  let(:test_case1) { double :rps, input: 'rock', c_choice: 'scissors'}
+  let(:test_case2) { double :rps, input: 'scissors', c_choice: 'paper'}
+  let(:test_case3) { double :rps, input: 'paper', c_choice: 'rock'}
+
+  it "Take the user's name when #get_name is called." do
     rps = RockPaperScissors.new
     allow(rps).to receive(:get_name) { 'Harry' }
     expect(rps.get_name).to eq('Harry')
   end
 
-  it "Should take the user's input when #user_input is called." do
+  it "Take the user's input when #user_input is called." do
     rps = RockPaperScissors.new
     allow(rps).to receive(:user_input) { 'rock' }
     expect(rps.user_input).to eq('rock')
   end
+
+  it 'Continue calling #user_input if input is invalid' do
+    rps = RockPaperScissors.new
+    allow(rps).to receive(:user_input) { 'invalid' }
+    expect(rps.user_input)
+  end
+
+  it 'The computer should make a random choice.' do
+    rps = RockPaperScissors.new
+    allow(rps).to receive(:computer_choice) { 'rock' }
+    expect(rps.computer_choice).to eq('rock')
+  end
+
+  it 'Declare tiebreak when selections are the same' do
+    allow(subject).to receive(:user_input) {'rock'}
+    allow(subject).to receive(:computer_choice).and_return('rock')
+    expect(subject.result(subject.user_input, subject.computer_choice)).to eq 'Tie.'
+  end
+
+  it 'Declare user winner if U: rock & C: scissors.' do
+    allow(subject).to receive(:user_input) {'rock'}
+    allow(subject).to receive(:computer_choice).and_return('scissors')
+    expect(subject.result(subject.user_input, subject.computer_choice)).to eq 'You win.'
+  end
+
+  it 'Declare user winner if U: scissors & C: paper' do
+    allow(subject).to receive(:user_input) {'scissors'}
+    allow(subject).to receive(:computer_choice).and_return('paper')
+    expect(subject.result(subject.user_input, subject.computer_choice)).to eq 'You win.'
+  end
+
+  it 'Declare user winner if U: paper & C: rock' do
+    allow(subject).to receive(:user_input) {'paper'}
+    allow(subject).to receive(:computer_choice).and_return('rock')
+    expect(subject.result(subject.user_input, subject.computer_choice)).to eq 'You win.'
+  end 
 end
